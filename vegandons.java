58c6133bb671acbda2bbd6c358c9dab51f44e5d4
bvindustries <120252887+bvindustries@users.noreply.github.com>
2022-12-15 03:32:55 +0000

    Language selector for start up

---
+++ vegandons.java
@@ -0,0 +1,30 @@
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+public class VeganDonApp {
+  public static void main(String[] args) {
+    // Set default language to English
+    Locale currentLocale = Locale.ENGLISH;
+
+    // Create a Scanner object for reading input from the user
+    Scanner scanner = new Scanner(System.in);
+
+    // Prompt the user to enter a language code
+    System.out.print("Enter language code (en, fr, etc.): ");
+    String languageCode = scanner.nextLine();
+
+    // Set the language to the specified locale
+    currentLocale = new Locale(languageCode);
+
+    // Load the resource bundle for the specified locale
+    ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", currentLocale);
+
+    // Get the value of the "greeting" key from the resource bundle
+    String greeting = messages.getString("greeting");
+
+    // Print the greeting to the console
+    System.out.println(greeting);
+  }
+}
+
